cf3ee8598f7ce3920e02f1857226605a3cf3d85b
santhoshKrishna <santhoshKrishna1702@gmail.com>
2022-06-18 06:37:50 +0530

    Dynamic Urls Part

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # path('sports',views.sports_view),
     # path('finance',views.finance_view)
 
-    path('<topics>/',views.dynamic_view),
-    path('<int:num1>/<int:num2>',views.sum_view)
+#     path('<str:topics>/',views.dynamic_view,name="topic_page"),
+#     # path('<int:num1>/<int:num2>',views.sum_view),                         ----->>>> Dynamic Urls Part 
+#     path('<int:numpage>/',views.num_page_view)
+# ]   
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,6 +1,6 @@
-from pydoc_data.topics import topics
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 # # Create your views here.
@@ -22,17 +22,32 @@ from django.http import HttpResponse
 # DynamicURLs
 
 
-articles = {
-    'sports':"This is sports View",
-    'finance':"This is finance View",
-    'politics':'"This is politics View"'
-}
+# articles = {
+#     'sports':"This is sports View",
+#     'finance':"This is finance View",
+#     'politics':'"This is politics View"'
+# }
 
-def dynamic_view(request,topics):
-    return HttpResponse(articles[topics])
+# def dynamic_view(request,topics):
+#     try:        
+#         return HttpResponse(articles[topics])
+#     except:
+#         # result = "Page is not found"
+#         # return HttpResponseNotFound(result)
+#         raise Http404("404 generic error")
+    
 
 
-def sum_view(reqest,num1,num2):
-    sum = num1 + num2
-    return HttpResponse(f'{num1} + {num2} = {sum} ')
+# def sum_view(reqest,num1,num2):
+#     sum = num1 + num2
+#     return HttpResponse(f'{num1} + {num2} = {sum} ')
+
+# def num_page_view(request,numpage):
+
+#     topic_list = list(articles.keys())
+#     topic = topic_list[numpage]
+
+#     return HttpResponseRedirect(reverse('topic_page',args=[numpage]))
+
+
 
